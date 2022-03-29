42bb6f162c42fb6849eee1cdf4eba09efb412bce
daniel <hellena171791@gmail.com>
2022-03-29 17:42:30 +0900

    <blazer DB #2> DB 접근 서비스 파일 추가

---
+++ BlazerRankingApp/Data/Services/RankingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazerRankingApp.Data.Services
+{
+    public class RankingService
+    {
+    }
+}
