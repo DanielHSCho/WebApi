03f2201d67b94bd9ff15627e2538336fa70011ed
daniel <hellena171791@gmail.com>
2022-03-30 17:35:15 +0900

    <WebApi #2> REST와 CRUD / WebApi의 RankingController 추가
    - WebAip는 Controller 기반
    - Rest 방식 사용
    - Create : POST
    - Read : GET
    - Update : PUT (웹에서는 보안문제로 사용하지 않음)
    - Delete : DELETE (웹에서는 보안문제로 사용하지 않음)

---
+++ WebApi/Controllers/RankingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
